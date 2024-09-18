f945f57331dc57b1ebc0e72a05982ce5c77c1ede
Kaio Jansen <54814187+Kaiojansen@users.noreply.github.com>
2024-09-18 20:56:18 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,31 +2,10 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Home() {
   return (
-    <main style={styles.main}>
-      <div style={styles.container}>
-        <Spline
-          scene="https://prod.spline.design/UGsVpzN8Fm-pzbRk/scene.splinecode"
-          style={styles.spline}
-        />
-      </div>
+    <main>
+      <Spline
+        scene="https://prod.spline.design/UGsVpzN8Fm-pzbRk/scene.splinecode" 
+      />
     </main>
   );
 }
-
-const styles = {
-  main: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh', // Ajusta conforme necessário
-  },
-  container: {
-    width: '100%',
-    maxWidth: '600px', // Define uma largura máxima para desktops
-    margin: '0 auto',
-  },
-  spline: {
-    width: '100%',
-    height: 'auto',
-  },
-};
