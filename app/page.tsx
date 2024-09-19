d1d8a447945705369aa57aaa30f1c964a568127a
Kaio Jansen <54814187+Kaiojansen@users.noreply.github.com>
2024-09-18 21:08:14 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,32 @@ import Spline from '@splinetool/react-spline/next';
 export default function Home() {
   return (
     <main>
-      <Spline
-        scene="https://prod.spline.design/UGsVpzN8Fm-pzbRk/scene.splinecode" 
-      />
+      <div className="spline-container">
+        <Spline
+          scene="https://prod.spline.design/UGsVpzN8Fm-pzbRk/scene.splinecode"
+        />
+      </div>
+      <style jsx>{`
+        /* Estilos padrão para desktop (opcional) */
+        .spline-container {
+          /* Estilos para desktop, se necessário */
+        }
+
+        /* Estilos específicos para dispositivos móveis */
+        @media only screen and (max-width: 767px) {
+          .spline-container {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+          }
+
+          .spline-container :global(canvas) {
+            display: block;
+            width: 100%;
+            height: 100%;
+          }
+        }
+      `}</style>
     </main>
   );
 }
